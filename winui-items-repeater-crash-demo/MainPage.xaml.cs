23eb485d08b0a00d3ef63adfeab1dce84f183d8d
Vincent Wang <376787823@qq.com>
2019-11-12 10:12:45 +0800

    Add files via upload

---
+++ winui-items-repeater-crash-demo/MainPage.xaml.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.UI.Xaml.Controls;
+using System.Collections.Generic;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace winui_items_repeater_crash_demo
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        private List<string> listName = new List<string>();
+        public MainPage()
+        {
+            this.InitializeComponent();
+
+            for (int i = 0; i < 100; i++)
+            {
+                listName.Add("Name: " + i.ToString());
+            }
+        }
+
+
+    }
+
+    public class NameS
+    {
+        public string name { get; set; }
+    }
+}
---
+++ winui-items-repeater-crash-demo/TextConverter.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.UI.Xaml.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace winui_items_repeater_crash_demo
+{
+    public class TextConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            if (value.ToString().Contains("0") || value.ToString().Contains("5"))
+                return value.ToString() + "   sss";
+            else
+                return value.ToString();
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
